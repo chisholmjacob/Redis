75d453ad1cbbfadef8b726727961e96eaae6f086
Jacob Chisholm <jchish9900@gmail.com>
2025-09-06 04:28:21 -0400

    setting up skeleton for redis

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../include/RedisServer.h"
+
+int main(int argc, char* argv){
+    int port = 6379;
+    if (argc >=2) port = std::stoi(argv[1]);
+
+    RedisServer server(port);
+
+
+    return 0;
+}
